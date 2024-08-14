255956efede25a65d224ea72a5181fde409b1d97
Wilson Vasquez <wilsonraymundov@gmail.com>
2024-08-14 01:18:49 -0600

    first commit

---
+++ Introduccion_Bootstrap/Introduccion_Bootstrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Introduccion_Bootstrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
